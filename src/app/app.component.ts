b52ed6f1aac7212f28400142bd326f93c82b2ca9
DESKTOP-95F3B96\dheeraj <dheerajb412@gmail.com>
2020-11-03 18:11:00 -0500

    Notes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+
+// @Component -- Decorators/Annotations
 @Component({
+  // Attributes
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'myApp';
+  // {{title}} -- string interpolation
+  // binds component properties to HTML5
+  title = 'MyApp works !';
 }
