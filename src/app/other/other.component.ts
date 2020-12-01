e572f9255575791a85d32c94491c079308bf5d8a
DESKTOP-95F3B96\dheeraj <dheerajb412@gmail.com>
2020-12-01 11:06:53 -0500

    Changes v1

--- src/app/other/other.component.ts
+++ src/app/other/other.component.ts
@@ -3,11 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   // # --> id selector
   selector: 'app-other',
+  // [id=app-other] - id selector
   // templateUrl: './other.component.html',
   // inline template
-  template: `<h2>
-    other works!
-  </h2>`,
+  template: `<ng-content>
+  </ng-content>`,
+  // `<h2>
+  //  other works!
+  // </h2>`,
   // styleUrls: ['./other.component.css']
   // inline style
   // encapsulation doesn't leaks or overrides the color with other component (It uses custom attribute)
